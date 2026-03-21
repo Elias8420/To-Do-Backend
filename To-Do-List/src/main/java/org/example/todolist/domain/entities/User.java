ff9f9840800f26ba186b0aaee9e114cc91948277
Elias8420 <eliaszelaya747@gmail.com>
2026-03-21 14:57:54 -0600

    add: User entitie

---
+++ To-Do-List/src/main/java/org/example/todolist/domain/entities/User.java
@@ -0,0 +1,30 @@
+package org.example.todolist.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
