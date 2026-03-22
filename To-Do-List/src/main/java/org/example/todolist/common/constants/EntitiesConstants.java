1e8c703989794f6d10f3af70204cc0c5c696e626
Elias8420 <eliaszelaya747@gmail.com>
2026-03-22 03:11:22 -0600

    add: entities constants

---
+++ To-Do-List/src/main/java/org/example/todolist/common/constants/EntitiesConstants.java
@@ -0,0 +1,9 @@
+package org.example.todolist.common.constants;
+
+public class EntitiesConstants {
+    public static final String USER_ENTITY = "User ";
+    public static final String TEAM_ENTITY = "Team ";
+    public static final String WORK_ENTITY = "Work ";
+    public static final String COMMENT_ENTITY = "Comment ";
+    public static final String ROLE_ENTITY = "Role ";
+}
