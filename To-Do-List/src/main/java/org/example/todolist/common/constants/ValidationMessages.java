f863a902d0eeeee44dd5dde57feda175b1557446
Elias8420 <eliaszelaya747@gmail.com>
2026-03-22 03:42:44 -0600

    add: message constant for validation in password and email fields

--- To-Do-List/src/main/java/org/example/todolist/common/constants/ValidationMessages.java
+++ To-Do-List/src/main/java/org/example/todolist/common/constants/ValidationMessages.java
@@ -3,4 +3,9 @@ package org.example.todolist.common.constants;
 public class ValidationMessages {
     public static final String USERNAME_EMPTY = "Username cannot be empty";
     public static final String USERNAME_PROFANITY = "Username cannot invalid words";
+    public static final String EMAIL_EMPTY = "Email cannot be empty";
+    public static final String INVALID_EMAIL = "Invalid email format";
+    public static final String PASSWORD_EMPTY = "Password cannot be empty";
+    public static final String NOT_LONG_PASSWORD = "Password must have 4 characters";
+    public static final String NOT_ALPHANUMERIC_PASSWORD = "Password must be alphanumeric";
 }
