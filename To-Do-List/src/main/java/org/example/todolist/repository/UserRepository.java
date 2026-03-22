1df67c4bf812ed56667031d2b091e178d8e417a2
Elias8420 <eliaszelaya747@gmail.com>
2026-03-22 17:22:32 -0600

    add: user repository

---
+++ To-Do-List/src/main/java/org/example/todolist/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package org.example.todolist.repository;
+
+import org.example.todolist.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
