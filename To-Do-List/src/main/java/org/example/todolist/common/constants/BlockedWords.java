cc13d908bacc9b02349971720b3ed111449332ad
Elias8420 <eliaszelaya747@gmail.com>
2026-03-22 03:08:52 -0600

    add: blocked words constants for validations

---
+++ To-Do-List/src/main/java/org/example/todolist/common/constants/BlockedWords.java
@@ -0,0 +1,13 @@
+package org.example.todolist.common.constants;
+
+public class BlockedWords {
+    public static final String BAD_WORD1 = "Puta";
+    public static final String BAD_WORD2 = "Puto";
+    public static final String BAD_WORD3 = "Mierda";
+    public static final String BAD_WORD4 = "Pendejo";
+    public static final String BAD_WORD5 = "Culero";
+    public static final String BAD_WORD6 = "Maricon";
+    public static final String BAD_WORD7 = "admin";
+    public static final String BAD_WORD8 = "superadmin";
+    public static final String BAD_WORD9 = "root";
+}
