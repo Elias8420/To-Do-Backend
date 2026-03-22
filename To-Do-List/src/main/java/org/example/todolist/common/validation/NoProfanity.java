591f94bd013c4b0ac64dc6757a36c3054725deb7
Elias8420 <eliaszelaya747@gmail.com>
2026-03-22 02:25:36 -0600

    refactor: replace text in class with a constant from constants file

--- To-Do-List/src/main/java/org/example/todolist/common/validation/NoProfanity.java
+++ To-Do-List/src/main/java/org/example/todolist/common/validation/NoProfanity.java
@@ -8,12 +8,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import static org.example.todolist.common.constants.ValidationMessages.USERNAME_PROFANITY;
+
 @Target({ElementType.FIELD}) // Le indica a spring que la anotacion se usa solo en atributos de clases
 @Retention(RetentionPolicy.RUNTIME) // Le indica a spring cuando existe la anotacion
 @Constraint(validatedBy = NoProfanityValidator.class) // Le indica a spring que clase contiene la logica de la anotacion
 // El @ antes del interface le indica a Java que queremos crear una anotacion
 public @interface NoProfanity {
-    String message() default "Username contains invalid words";
+    String message() default USERNAME_PROFANITY;
     // Mensaje de error que se muestra por defecto si no cumple con la validacion
 
     Class<?>[] groups() default {};
