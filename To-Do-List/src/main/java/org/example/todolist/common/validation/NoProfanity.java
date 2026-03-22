ba9985b9cbc7f3392be304eaaa08c9dca8750fbd
Elias8420 <eliaszelaya747@gmail.com>
2026-03-22 02:24:31 -0600

    docs: add commente to no profanity class

--- To-Do-List/src/main/java/org/example/todolist/common/validation/NoProfanity.java
+++ To-Do-List/src/main/java/org/example/todolist/common/validation/NoProfanity.java
@@ -8,13 +8,17 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.FIELD})
-@Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = NoProfanityValidator.class)
+@Target({ElementType.FIELD}) // Le indica a spring que la anotacion se usa solo en atributos de clases
+@Retention(RetentionPolicy.RUNTIME) // Le indica a spring cuando existe la anotacion
+@Constraint(validatedBy = NoProfanityValidator.class) // Le indica a spring que clase contiene la logica de la anotacion
+// El @ antes del interface le indica a Java que queremos crear una anotacion
 public @interface NoProfanity {
     String message() default "Username contains invalid words";
+    // Mensaje de error que se muestra por defecto si no cumple con la validacion
 
     Class<?>[] groups() default {};
+    // Permite agrupar validaciones
 
     Class<? extends Payload>[] payload() default {};
+    // Permite agregar metadata a la validacion
 }
