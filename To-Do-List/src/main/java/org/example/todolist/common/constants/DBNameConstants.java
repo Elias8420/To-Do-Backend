e9ef44fdf8c6b20b1bdadad25f265b1fb80b3d89
Elias8420 <eliaszelaya747@gmail.com>
2026-03-22 03:41:31 -0600

    add: constants for fields and table names in DB

---
+++ To-Do-List/src/main/java/org/example/todolist/common/constants/DBNameConstants.java
@@ -0,0 +1,9 @@
+package org.example.todolist.common.constants;
+
+public class DBNameConstants {
+    public static final String USERNAME = "name";
+    public static final String USEREMAIL = "email";
+    public static final String USERPASSWORD = "password";
+    public static final String USERIMAGE = "image";
+    public static final String USERTABLE = "User";
+}
