8486506ccc62ae21400fe215333ad3aea3b16d79
Elias8420 <eliaszelaya747@gmail.com>
2026-03-22 02:18:11 -0600

    add: constant for profanity username

--- To-Do-List/src/main/java/org/example/todolist/common/constants/ValidationMessages.java
+++ To-Do-List/src/main/java/org/example/todolist/common/constants/ValidationMessages.java
@@ -2,4 +2,5 @@ package org.example.todolist.common.constants;
 
 public class ValidationMessages {
     public static final String USERNAME_EMPTY = "Username cannot be empty";
+    public static final String USERNAME_PROFANITY = "Username cannot invalid words";
 }
