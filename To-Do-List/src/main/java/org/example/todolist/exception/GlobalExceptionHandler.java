0f58da2392ae9ecce86bec07943343f219f5dbbc
Elias8420 <eliaszelaya747@gmail.com>
2026-03-22 19:19:23 -0600

    add: generalk respponse for exceptions

---
+++ To-Do-List/src/main/java/org/example/todolist/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package org.example.todolist.exception;
+
+import org.example.todolist.domain.dto.response.user.ApiErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.time.LocalDate;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    public ResponseEntity<ApiErrorResponse> buildErrorResponse(Exception e, HttpStatus status, Object data) {
+        String uri = ServletUriComponentsBuilder.fromCurrentRequestUri().build().getPath();
+
+        return ResponseEntity
+                .status(status)
+                .body(ApiErrorResponse.builder()
+                        .status(status.value())
+                        .message(data)
+                        .time(LocalDate.now())
+                        .uri(uri)
+                        .build()
+                );
+    }
+}
