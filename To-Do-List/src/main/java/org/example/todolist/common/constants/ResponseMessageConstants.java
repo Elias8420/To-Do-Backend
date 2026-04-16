d4d4dbc596032698d4e8c22e19e4bd5279e462e7
Elias8420 <eliaszelaya747@gmail.com>
2026-04-16 15:48:35 -0600

    add: response constants

---
+++ To-Do-List/src/main/java/org/example/todolist/common/constants/ResponseMessageConstants.java
@@ -0,0 +1,7 @@
+package org.example.todolist.common.constants;
+
+public class ResponseMessageConstants {
+    public static final String USER = "User ";
+    public static final String CREATED = "created ";
+    public static final String SUCCESS = "successfully";
+}
