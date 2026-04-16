723e721b94907adb857b5e9025aeb1962ee55085
Elias8420 <eliaszelaya747@gmail.com>
2026-04-16 15:47:49 -0600

    add: constants for endpoints

---
+++ To-Do-List/src/main/java/org/example/todolist/common/constants/EndpointsConstants.java
@@ -0,0 +1,7 @@
+package org.example.todolist.common.constants;
+
+public class EndpointsConstants {
+    public static final String BASE_URL = "/api";
+    public static final String USERS_ENDPOINT = "/users";
+    public static final String CREATE_PATH = "/create";
+}
