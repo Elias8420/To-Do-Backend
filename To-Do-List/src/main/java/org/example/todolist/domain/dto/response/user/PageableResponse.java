fa64c94bb75c3aeeafcf31da7fccad5e77304741
Elias8420 <eliaszelaya747@gmail.com>
2026-04-29 17:36:19 -0600

    add: pageable response for get all resources

--- To-Do-List/src/main/java/org/example/todolist/domain/dto/response/user/PageableResponse.java
+++ To-Do-List/src/main/java/org/example/todolist/domain/dto/response/user/PageableResponse.java
@@ -1,18 +1,22 @@
 package org.example.todolist.domain.dto.response.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 //Agregar el <T> convierte a la clase en una clase generica
 public class PageableResponse<T> {
     private List<T> content;
     private int page;
     private int size;
-    private int totalElements;
+    private long totalElements;
     private int totalPages;
     private boolean last;
 }
