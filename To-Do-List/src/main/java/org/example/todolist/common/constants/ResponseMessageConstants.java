14ed8acd1511a668c57f241e42cf3dbfcbcd57ec
Elias8420 <eliaszelaya747@gmail.com>
2026-04-29 17:38:28 -0600

    add: constant when found a resources

--- To-Do-List/src/main/java/org/example/todolist/common/constants/ResponseMessageConstants.java
+++ To-Do-List/src/main/java/org/example/todolist/common/constants/ResponseMessageConstants.java
@@ -4,4 +4,5 @@ public class ResponseMessageConstants {
     public static final String USER = "User ";
     public static final String CREATED = "created ";
     public static final String SUCCESS = "successfully";
+    public static final String FOUND = "found";
 }
