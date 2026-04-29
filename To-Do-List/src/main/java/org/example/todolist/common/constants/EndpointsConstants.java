af4083810539dca01d2ef85fd0320b77d891f203
Elias8420 <eliaszelaya747@gmail.com>
2026-04-29 17:39:49 -0600

    add: constant to get all resources in endpoints

--- To-Do-List/src/main/java/org/example/todolist/common/constants/EndpointsConstants.java
+++ To-Do-List/src/main/java/org/example/todolist/common/constants/EndpointsConstants.java
@@ -4,4 +4,5 @@ public class EndpointsConstants {
     public static final String BASE_URL = "/api";
     public static final String USERS_ENDPOINT = "/users";
     public static final String CREATE_PATH = "/create";
+    public static final String GET_ALL_PATH = "/getAll";
 }
