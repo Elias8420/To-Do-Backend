e525d718dc3bb29bca79c565844c39c05441aed8
Elias8420 <eliaszelaya747@gmail.com>
2026-04-29 17:42:35 -0600

    add: controller to get all users paged

--- To-Do-List/src/main/java/org/example/todolist/controller/UserController.java
+++ To-Do-List/src/main/java/org/example/todolist/controller/UserController.java
@@ -4,13 +4,15 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.example.todolist.domain.dto.request.user.CreateUserRequest;
 import org.example.todolist.domain.dto.response.user.GeneralResponse;
+import org.example.todolist.domain.dto.response.user.PageableResponse;
+import org.example.todolist.domain.dto.response.user.UserResponse;
 import org.example.todolist.service.impl.UserServiceImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.time.LocalDateTime;
@@ -28,6 +30,22 @@ public class UserController {
     public ResponseEntity<GeneralResponse> createUser(@RequestBody @Valid CreateUserRequest createUserRequest) {
         userService.createUser(createUserRequest);
         return buildResponse(USER+CREATED+SUCCESS, HttpStatus.CREATED, null);
+    }
+
+    @GetMapping(GET_ALL_PATH)
+    public ResponseEntity<GeneralResponse> getAllUsers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String sortOrder
+    ) {
+        Sort sort = sortOrder.equalsIgnoreCase("desc") ?
+                Sort.by(sortBy).descending() :
+                Sort.by(sortBy).ascending();
+
+        Pageable pageable = PageRequest.of(page, size, sort);
+        PageableResponse<UserResponse> pagedResponse = userService.getAllUsers(pageable);
+        return buildResponse(USER+FOUND, HttpStatus.FOUND, pagedResponse);
     }
 
     public ResponseEntity<GeneralResponse> buildResponse(String message, HttpStatus status, Object data) {
