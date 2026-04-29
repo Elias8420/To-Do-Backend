f9b3110bef85d74cc1c210438b7935f02805f90e
Elias8420 <eliaszelaya747@gmail.com>
2026-04-29 17:37:45 -0600

    add: constant when any resource's not found

--- To-Do-List/src/main/java/org/example/todolist/common/constants/ExceptionsMessageConstants.java
+++ To-Do-List/src/main/java/org/example/todolist/common/constants/ExceptionsMessageConstants.java
@@ -2,4 +2,5 @@ package org.example.todolist.common.constants;
 
 public class ExceptionsMessageConstants {
     public static final String ALREADY_EXIST = "already exist";
+    public static final String NOT_FOUND = "not found";
 }
