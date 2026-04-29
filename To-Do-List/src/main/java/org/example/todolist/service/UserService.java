72a4c17661c9f5ee7dda634270d0802285f219b0
Elias8420 <eliaszelaya747@gmail.com>
2026-04-29 17:41:56 -0600

    add: service to get all users

--- To-Do-List/src/main/java/org/example/todolist/service/UserService.java
+++ To-Do-List/src/main/java/org/example/todolist/service/UserService.java
@@ -1,7 +1,11 @@
 package org.example.todolist.service;
 
 import org.example.todolist.domain.dto.request.user.CreateUserRequest;
+import org.example.todolist.domain.dto.response.user.PageableResponse;
+import org.example.todolist.domain.dto.response.user.UserResponse;
+import org.springframework.data.domain.Pageable;
 
 public interface UserService {
     void createUser(CreateUserRequest createUserRequest);
+    PageableResponse<UserResponse> getAllUsers(Pageable pageable);
 }
